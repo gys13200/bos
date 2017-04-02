c857c32a058089420b85ec7d2de8188996c2b463
gys <gys13200@163.com>
2017-04-02 22:10:17 +0800

    first commit

---
+++ src/main/java/cn/itcast/bos/web/controller/FunctionController.java
@@ -0,0 +1,38 @@
+package cn.itcast.bos.web.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.itcast.bos.domain.Function;
+import cn.itcast.bos.domain.User;
+import cn.itcast.bos.service.FunctionService;
+
+/**
+ * 权限操作
+ * 
+ * @author seawind
+ * 
+ */
+@Controller
+public class FunctionController {
+
+	@Autowired
+	private FunctionService functionService;
+
+	@RequestMapping("/function_menu.do")
+	@ResponseBody
+	public Object menu(HttpSession session) {
+		// 查询当前用户具有功能
+		User user = (User) session.getAttribute("user");
+
+		// 调用业务层 ，返回Function对象集合
+		List<Function> functions = functionService.findMenu(user);
+		return functions;
+	}
+}
